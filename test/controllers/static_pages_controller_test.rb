007fa20a00dcfd2ff03ba82902c630750596ca03
kask666 <kazimierz.bik@gmail.com>
2014-12-20 22:18:32 +0000

    Finished till 3.4.3 Layouts and embedded Ruby (Refactor)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
+  end
+  
+  test "home should have correct title" do
+    get :home
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -11,4 +17,19 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "help should have correct title" do
+    get :help
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  
+  test "about ahould have correct title" do
+    get :about
+    assert_select "title", "About  | Ruby on Rails Tutorial Sample App"
+  end 
+  
 end
