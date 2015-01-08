f44bf0372faace8ab3e408cf7e8170e8b7129ab2
kask666 <kazimierz.bik@gmail.com>
2015-01-08 20:55:22 +0000

    Finished till listing 6.30

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -40,11 +40,21 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email validation should reject invalid addresses" do
+    # %[...] tworzy array stringów
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
+      # druga zmienna to opcjonalny komentarz jesli test sie nie powiedzie
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+  
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
 end
