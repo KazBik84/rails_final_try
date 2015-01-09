250ff718262fee0555cef7b055a7f371ee98e3ff
kask666 <kazimierz.bik@gmail.com>
2015-01-09 19:56:40 +0000

    Finished till 6.3.2 User has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  # call back - czyli procedura ktora zostanie wykonana 
+  #             przed wykonaniem akcji w tym przypadku
+  #             przed save
+  before_save { self.email = email.downcase }
   validates :name, { presence: true, length: { maximum: 50 } }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
   validates :email, { presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false } }
+  has_secure_password
 end
