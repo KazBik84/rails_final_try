e5fb43fac83428a431faf1396005eb82c8c4ca3c
kask666 <kazimierz.bik@gmail.com>
2015-01-12 07:09:50 +0000

    Finished at 7.1.2 A user resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   # call back - czyli procedura ktora zostanie wykonana 
   #             przed wykonaniem akcji w tym przypadku
   #             przed save
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }# to to samo co => self.email = email.downcase 
   validates :name, { presence: true, length: { maximum: 50 } }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, { presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false } }
