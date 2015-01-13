258c477e3c681945ef718286557444f53674fd67
kask666 <kazimierz.bik@gmail.com>
2015-01-13 17:32:24 +0000

    Finished until 7.2.2 Signup form HTML

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  #Zwraca Gravatara dla konkretnego usera
+  def gravatar_for(user)
+    # koduje wartość user.email.downcase, przy pomocy kodu MD%
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # Tworzy link do gravatara dodając na koniec zakodowaną wartość maila
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
