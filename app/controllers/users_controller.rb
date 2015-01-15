f58999a4995fbadf4c60098f5ec01033cbd7b4e5
kask666 <kazimierz.bik@gmail.com>
2015-01-15 10:47:32 +0000

    Finished till 7.3.3 Sign up error massages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,27 @@ class UsersController < ApplicationController
   end
   
   def new
+    #tworzy nowy obiekt i przypisuje go do zmiennej @user
     @user = User.new
   end
   
+  def create 
+    #tworzy nowy obiekt @user i przypisuje do niego wartości zapisane w user_params 
+    #które otrzymał po wypełnionym formularzu w akcji 'new'
+    @user = User.new(user_params)
+    if @user.save #jesli uda sie zapisac obiekt do bazy danych
+      #tralalala
+    else
+      render 'new' #przekierowuje do akcji 'new'
+    end
+  end
+  
+  private # wszystko po tej komendzie jest prywatne tylko dla users_controller
+  
+    #funkcja ta przyjmie wartości przesłane w params[:user] 
+    #i zwróci tylko dopuszczone wartości
+    def user_params
+     params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation) 
+    end
 end
