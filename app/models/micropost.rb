826c9b10cf4264f61f2114ee1d31235e3432ca2a
KazikBik <kazimierz.bik@gmil.com>
2015-03-09 20:09:05 -0300

    Skończyłem do 11.4.3 dobrze idzie :)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -8,6 +8,26 @@ class Micropost < ActiveRecord::Base
   #		w tym przypdaku kolejność jest ustalana wg. atrybutu 'created_at', 
   #		a symbol ':desc' oznacza, że kolejność będzie malejąca
   default_scope -> { order(created_at: :desc) }
+  # Funkcja mount_uploader pochodzi z gema CarrierWave, odpowiedzialnego
+  #		za upload obrazów.
+  # By połączyć obrazek z obiektem modelu należy podać nazwę atrybutu(modelu), 
+  #		do któej zostanie zapisany, jako symbol ':picture' oraz nazwę uploadera 
+  #		'PictureUploader', która zdefiniowana jest w app/uploader/picture_uploader
+  #	Plik picture uploader został wygenerowany poprzez generator
+  #		'rails generate uploader Picture', gdzie picture to nazwa zmiennej. 
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  # !!!! validate to nie to samo co validates !!!!
+  #		validate oznacza wlasna walidacje, którą nalezy zdefiniować
+  validate :picture_size
+  
+  private
+  	
+  	# Sprawdza rozmiar pliku
+  	def picture_size
+  		if picture.size > 5.megabytes
+  			errors.add(:picture, "should be less than 5MB")
+  		end
+  	end
 end
